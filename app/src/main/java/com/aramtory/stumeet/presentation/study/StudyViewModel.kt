82ef221049f290398a32fb46271b502e8fdb6a02
yerim1ee <yearim1226@naver.com>
2024-12-25 03:56:01 +0900

    comment: 동작 에러나는 부분 주석 처리 (#3)

--- app/src/main/java/com/aramtory/stumeet/presentation/study/StudyViewModel.kt
+++ app/src/main/java/com/aramtory/stumeet/presentation/study/StudyViewModel.kt
@@ -1,25 +1,23 @@
 package com.aramtory.stumeet.presentation.study
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.aramtory.stumeet.data.ApiPool
-import kotlinx.coroutines.launch
 
 class StudyViewModel : ViewModel() {
     private val _getSomething: MutableLiveData<Unit?> = MutableLiveData()
     val getSomething: MutableLiveData<Unit?> = _getSomething
 
-    fun getTodaySwubab(time: String) = viewModelScope.launch {
-        runCatching {
-            ApiPool.getStudy.getSomething()
-        }.fold(
-            {
-                _getSomething.value = it.data
-            }, {
-                it.message?.let { it1 -> Log.d("stumeet", it1) }
-            }
-        )
+    /**
+    fun getSomething(time: String) = viewModelScope.launch {
+    runCatching {
+    BaseApiPool.getStudyActivity.getSomething()
+    }.fold(
+    {
+    _getSomething.value = it.data
+    }, {
+    it.message?.let { it1 -> Log.d("stumeet", it1) }
     }
+    )
+    }
+     **/
 }
