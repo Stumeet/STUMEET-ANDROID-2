0d8ce0dfe9d10aef639bafb2b04109ed12afc0d1
yerim1ee <yearim1226@naver.com>
2024-12-25 04:09:30 +0900

    feat: 마이페이지 관련 api 요청 dto 추가 (#3)

---
+++ app/src/main/java/com/aramtory/stumeet/data/dto/req/my/ReviewReqDto.kt
@@ -0,0 +1,19 @@
+package com.aramtory.stumeet.data.dto.req.my
+
+import com.aramtory.stumeet.data.dto.common.enums.ReviewTagType
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ReviewReqDto(
+    @SerialName("content")
+    val content: String,
+    @SerialName("rate")
+    val rate: Int,
+    @SerialName("reviewTags")
+    val reviewTags: List<ReviewTagType>,
+    @SerialName("revieweeId")
+    val revieweeId: Int,
+    @SerialName("studyId")
+    val studyId: Int
+)
