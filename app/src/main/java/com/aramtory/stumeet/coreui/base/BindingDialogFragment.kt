4e00dece6c9105e5f50114b2c115b8c594154e8e
yerim1ee <yearim1226@naver.com>
2024-12-27 01:29:16 +0900

    feat: ViewDataBinding을 위한 DialogFragment 기본 클래스 추가 (#5)

---
+++ app/src/main/java/com/aramtory/stumeet/coreui/base/BindingDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.aramtory.stumeet.coreui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+
+abstract class BindingDialogFragment<T : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : DialogFragment() {
+    private var _binding: T? = null
+    protected val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    protected abstract fun initView()
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
