15ca98cc7ed82c7d06bb87e345fa0f458aef1858
yerim1ee <yearim1226@naver.com>
2024-12-27 14:53:05 +0900

    fix: fragment binding 클래스에 init 메서드 추가 (#11)

--- app/src/main/java/com/aramtory/stumeet/coreui/base/BindingFragment.kt
+++ app/src/main/java/com/aramtory/stumeet/coreui/base/BindingFragment.kt
@@ -10,26 +10,33 @@ import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
 abstract class BindingFragment<T : ViewDataBinding>(
-    @LayoutRes private val layoutRes: Int,
+    @LayoutRes private val layoutRes: Int
 ) : Fragment() {
     private var _binding: T? = null
-    protected val binding get() = requireNotNull(_binding) {
-
-    }
+    protected val binding
+        get() = requireNotNull(_binding) {
+            "binding object is not initialized"
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    protected abstract fun initView()
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 }
