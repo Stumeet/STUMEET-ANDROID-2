551e1ac4563dc5669e70e5ee1823317982abd0ee
youjin09222 <youjin09222@naver.com>
2025-01-01 01:35:32 +0900

    #21 [REFACTOR] LoginViewModel에 UiState 및 서버 통신 로직 추가

--- app/src/main/java/com/aramtory/stumeet/presentation/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/aramtory/stumeet/presentation/auth/login/LoginViewModel.kt
@@ -2,30 +2,47 @@ package com.aramtory.stumeet.presentation.auth.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.user.UserApiClient
+import com.aramtory.stumeet.coreui.view.UiState
+import com.aramtory.stumeet.data.SharedManager
+import com.aramtory.stumeet.data.api.signup.SignUpApiService
+import com.aramtory.stumeet.data.dto.res.signup.AccessTokenResDto
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
-class LoginViewModel(private val kakaoLoginService: KakaoLoginService) : ViewModel() {
-    private val _isKakaoLogin = MutableStateFlow(false)
-    val isKakaoLogin = _isKakaoLogin.asStateFlow()
+class LoginViewModel(
+    private val kakaoLoginService: KakaoLoginService,
+    private val signUpApiService: SignUpApiService
+) : ViewModel() {
 
-    val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        KakaoLoginCallback {
-            _isKakaoLogin.value = true
-        }.handleResult(token, error)
+    private val _loginState = MutableStateFlow<UiState<AccessTokenResDto>>(UiState.Empty)
+    val loginState: StateFlow<UiState<AccessTokenResDto>> = _loginState
 
-        UserApiClient.instance.me { user, meError ->
-            if (meError != null) {
-                Timber.e("KakaoLogin", "사용자 정보 요청 실패", meError)
-            }
+    fun kakaoLogin() {
+        _loginState.value = UiState.Loading
+        kakaoLoginService.startKakaoLogin { oAuthToken, error ->
+            error?.let {
+                _loginState.value = UiState.Failure("카카오 로그인 실패: ${it.localizedMessage}")
+            } ?: oAuthToken?.accessToken?.let(::fetchAccessTokenFromServer)
         }
     }
 
-    fun kakaoLogin() = viewModelScope.launch {
-        kakaoLoginService.startKakaoLogin(kakaoLoginCallback)
+    private fun fetchAccessTokenFromServer(oauthToken: String) = viewModelScope.launch {
+        _loginState.value = try {
+            val response = signUpApiService.postAccessToken("Bearer $oauthToken")
+            response.data?.let {
+                saveTokens(it)
+                UiState.Success(it)
+            } ?: UiState.Failure("서버 응답이 올바르지 않습니다.")
+        } catch (e: Exception) {
+            UiState.Failure("서버 요청 실패: ${e.localizedMessage}")
+        }
+    }
+
+    private fun saveTokens(accessTokenResDto: AccessTokenResDto) {
+        SharedManager.saveTokens(
+            accessToken = accessTokenResDto.accessToken,
+            refreshToken = accessTokenResDto.refreshToken
+        )
     }
 }
